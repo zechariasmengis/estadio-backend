5ad4d0465cd86cd4455f970c8c4ef9b1a6661eb4
Zecharias Mengis <zmengis@gmail.com>
2021-05-11 14:11:20 -0500

    added model relationships

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -1,2 +1,4 @@
 class Stadium < ApplicationRecord
+    has_many :visits
+    has_many :users, through: :visits
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :visits
+    has_many :stadia, through: :visits
 end
